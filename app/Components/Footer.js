9b190b035813d467a10d9378c0632600af3d4d19
Md.Emon <sheikhmdemon540@gmail.com>
2025-01-28 20:35:35 +0600

    header component is ready

--- app/Components/Footer.js
+++ app/Components/Footer.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer className="bg-[#121212] footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white ">
+            <div className="container p-12 flex justify-between">
+                <span>LOGO</span>
+                <p className="text-slate-600 text-yellow-400">All rights reserved for Md. Emon Sheikh.</p>
+            </div>
+        </footer>
     );
 };
 
